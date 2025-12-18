c9aa2f750f9082a69b8862076f624d7773fb7b7a
Appatechlab <appabenedict@gmail.com>
2025-12-18 09:01:58 +0000

    base url changed

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1";
-// const BASE_URL = "https://asante-nupsg-backend.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://asante-nupsg-backend.onrender.com/api/v1";
 
 export const BASE_API = axios.create({
   baseURL: BASE_URL,
